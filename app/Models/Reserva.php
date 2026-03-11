04dd539c084c399ac832f70638158cbf9501905d
Marcos <go25001@itcha.edu.sv>
2026-03-11 09:22:51 -0600

    Revert "ReservaController"
    This reverts commit fb90486fca1f700bdc5e4289ac7eb43e9e4af4da.

--- app/Models/Reserva.php
+++ app/Models/Reserva.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Reserva extends Model
 {
@@ -14,20 +13,15 @@ class Reserva extends Model
         'fecha_entrada',
         'fecha_salida',
         'estado',
-        'user_id',//en esta parte hacia falta el id de user
-    ];
-
+    ]; 
+     
     protected $casts = [
         'total_precio' => 'decimal:2',
         'fecha_reserva' => 'datetime',
         'fecha_entrada' => 'date',
         'fecha_salida' => 'date',
-        'estado' => 'string'//aqui basandome en la base de datos lo canbie por string ya que en la base de datos tiene varchar"50"
+        'estado' => 'bool'
     ];
-    public function detalles(): HasMany
-    {
-        return $this->hasMany(DetalleReserva ::class,'reserva_id');//agregaue el puente qeu permite la comunicacion de las tablas
-    }
 
     public function user():BelongsTo
     {
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ReservaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,9 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::apiResource('reservas', ReservaController::class);
-
 
 
 
